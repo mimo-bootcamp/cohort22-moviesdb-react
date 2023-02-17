742cdad35e571eaedf0d1de559ea27393192eeb3
Aaron <aaron1venema@gmail.com>
2023-02-16 21:26:01 -0600

    Our Team

---
+++ src/data/bios.jsx
@@ -0,0 +1,17 @@
+const teamMembers = [
+    {
+        name: "Aaron Venema",
+        pictureURL: "/assets/aaron.jpg",
+        bio: "While enjoying my leisure time, you’ll typically find me listening to audiobooks or podcasts centered around science/technology/history, or drenched in the sauna getting my daily hormesis.",
+        website: "aaronvenema.com",
+        id: 1,
+    },{
+        name: "Alex Hessler",
+        pictureURL: "/assets/alex.jpg",
+        bio: "I'm a Web Developer who is never done learning, developing and growing. My strongest focuses revolve around team cohesiveness and the removal of all barriers for any potential challenges.",
+        website: "alexrhessler.com",
+        id: 2
+    }
+]
+
+export default teamMembers;
--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,9 +1,23 @@
-
+import data from "../data/bios"
 
 const Team = () => {
+
+
+
     return ( 
-        <section>
-            <h1>Team</h1>
+        <section className="team-section">
+            <h1>Our Team</h1>
+            <div className="cards-container">
+            {data.map((item) => (
+                <article className="bio-card">
+                    <img src={item.pictureURL} alt={item.name} className="bio"/>
+                    <h3>{item.name}</h3>
+                    <a href={item.website} target="_blank" rel="noreferrer" >{item.website}</a>
+                    <p className="bio">{item.bio}</p>
+                </article>
+            ))}
+            </div>
+
         </section>
      );
 }
