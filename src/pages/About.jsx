7af1e6a272a491ee4e589b6fcf78e504e18f4a9c
twistedstack <kelly@getmimo.com>
2023-02-21 09:04:10 -0500

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,6 +4,7 @@ const About = () => {
     return ( 
         <section className="about-section">
             <h2>About</h2>    
+            <p>Coming Soon</p>
         </section>
         
      );
